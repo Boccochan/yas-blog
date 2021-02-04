9964f5de6abb5f63d6e217c2f5ace1d32f054b0b
Boccochan <boccochan@gmail.com>
2021-02-05 00:50:01 +0900

    Delete unused code

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,9 +31,6 @@ module.exports = {
         },
         `gatsby-plugin-typescript`,
         `gatsby-plugin-postcss`,
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
         {
             resolve: `gatsby-plugin-alias-imports`,
             options: {
