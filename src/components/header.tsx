78f9d3527cd9c526bb4ae18069bdbf1da6f3edd9
Boccochan <boccochan@gmail.com>
2021-02-05 01:13:38 +0900

    Add separator to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import { Link } from 'gatsby';
 import React from 'react';
 import { FunctionComponent } from 'react';
 import { ReactElement } from 'react';
+import '@/styles/header.css';
 
 interface HeaderProps {
     siteTitle: string;
@@ -12,12 +13,12 @@ const Header: FunctionComponent<HeaderProps> = ({
 }: HeaderProps): ReactElement => (
     <header className="bg-white mb-6">
         <div className="container mx-auto max-w-6xl py-2 px-2">
-            <h1 className="text-2xl inline-block text-center p-2">
+            <h1 className="text-2xl inline-block text-center py-1 mr-6">
                 <Link to="/" className="text-gray-800">
                     {siteTitle}
                 </Link>
             </h1>
-            <div className="inline-block p-2">
+            <div className="inline-block border-l-2 border-black px-6">
                 <Link to="/blog" className="text-gray-800 text-center">
                     Yasuhiro Official Blog
                 </Link>
