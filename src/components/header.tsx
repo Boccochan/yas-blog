b7f239cb073c6538380b22ba86f7a131f7bb2d98
Boccochan <boccochan@gmail.com>
2021-02-06 00:57:16 +0900

    Add flex box

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,13 +11,13 @@ const Header: FunctionComponent<HeaderProps> = ({
     siteTitle,
 }: HeaderProps): ReactElement => (
     <header className="bg-white mb-6">
-        <div className="container mx-auto max-w-6xl py-2 px-2">
-            <h1 className="text-2xl inline-block text-center py-1 mr-6">
+        <div className="flex flex-row items-center mx-auto max-w-6xl py-2 px-2">
+            <h1 className="text-2xl ext-center py-1 mr-6">
                 <Link to="/" className="text-gray-800">
                     {siteTitle}
                 </Link>
             </h1>
-            <div className="inline-block border-l-2 border-black px-6">
+            <div className="border-l-2 border-black px-6">
                 <Link to="/blog" className="text-gray-800 text-center">
                     Yasuhiro Official Blog
                 </Link>
