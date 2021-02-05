e82367e9929393e593c13fb0d506c36551e5b816
Boccochan <boccochan@gmail.com>
2021-02-06 08:47:56 +0900

    Change nav to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,36 +12,37 @@ const Header: FunctionComponent<HeaderProps> = ({
     siteTitle,
 }: HeaderProps): ReactElement => (
     <header className="bg-white mb-6">
-        <div className="flex flex-row items-center mx-auto max-w-6xl py-2 px-2">
+        <nav className="flex flex-row items-center mx-auto max-w-6xl py-2 px-2">
             <h1 className="text-2xl text-center py-1 mr-6">
                 <Link to="/" className="text-gray-800">
                     {siteTitle}
                 </Link>
             </h1>
-            <div className="flex flex-row w-6/12">
-                <div className="border-l-2 border-black px-6">
+
+            <ul className="flex flex-row w-6/12">
+                <li className="border-l-2 border-black px-6">
                     <Link to="/blog" className="text-gray-900 text-center menu">
                         Yasuhiro Official Blog
                     </Link>
-                </div>
-                <div>
+                </li>
+                <li>
                     <Link
                         to="/lab"
                         className="text-gray-900 text-center mr-6 menu font-light text-sm"
                     >
                         Laboratory
                     </Link>
-                </div>
-                <div>
+                </li>
+                <li>
                     <Link
                         to="/contact"
                         className="text-gray-900 text-center menu font-light text-sm"
                     >
                         Contact
                     </Link>
-                </div>
-            </div>
-        </div>
+                </li>
+            </ul>
+        </nav>
     </header>
 )
 
