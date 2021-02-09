d58ff35967a5507d02c16004c7ff0a5f86db2c23
Boccochan <boccochan@gmail.com>
2021-02-09 23:30:24 +0900

    Refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Image from '@/components/image'
-import SEO from '@/components/seo'
-import Layout from '@/components/layout'
+import Image from '@/components/Image'
+import SEO from '@/components/Seo'
+import Layout from '@/components/Layout'
 import { useTranslation } from 'react-i18next'
 
 const IndexPage = () => {
