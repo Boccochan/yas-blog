159c04c6e2e6e344c48d1d9ff7e5bfadc3f79247
Yasuhiro <boccochan@gmail.com>
2021-02-24 22:06:33 +0900

    Fix selected a menu link

--- src/components/Anchor.tsx
+++ src/components/Anchor.tsx
@@ -13,10 +13,11 @@ const Anchor = ({ to, children, className }: Props) => {
   const { lang } = usePageContext()
   const { pathname } = useLocation()
 
-  console.log(222, lang)
-
   const nextPath = `/${lang}${to}`
-  const style = pathname === nextPath ? `${className} text-blue-700` : className
+  const style =
+    pathname.split('/')[2] === to.split('/')[1]
+      ? `${className} text-blue-700 font-normal`
+      : `${className} font-normal`
 
   return (
     <Link to={nextPath} className={style}>
