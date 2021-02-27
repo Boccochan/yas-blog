4ca774b36a70659b6441dcda708be430e543da4d
Yasuhiro <boccochan@gmail.com>
2021-02-27 11:49:36 +0900

    Refactor

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -5,7 +5,12 @@ import Bio from '@/components/Blog/Bio'
 import Summaries, { SummaryProps } from '@/components/Blog/Summaries'
 import { useTranslation } from 'react-i18next'
 import { graphql, PageRendererProps } from 'gatsby'
-import { Query, SitePageContext } from '@/types/graphql-types'
+import {
+  Query,
+  SitePageContext,
+  MarkdownRemark,
+  MarkdownRemarkEdge,
+} from '@/types/graphql-types'
 
 interface Props extends PageRendererProps {
   pageContext: SitePageContext
@@ -44,88 +49,64 @@ export const pageQuery = graphql`
   }
 `
 
+const createSummary = (node: MarkdownRemark, descript: boolean = false) => {
+  const { title, date, description } = node.frontmatter!
+  if (descript) {
+    return {
+      title: title as string,
+      date: date as string,
+      description: description as string,
+      slug: node!.fields!.slug as string,
+      img: node!.frontmatter!.featuredImage.childrenImageSharp[0].fluid,
+    }
+  }
+  return {
+    title: title as string,
+    date: date as string,
+    slug: node!.fields!.slug as string,
+    img: node!.frontmatter!.featuredImage.childrenImageSharp[0].fluid,
+  }
+}
+
+const getBlogs = (
+  edges: MarkdownRemarkEdge[],
+  start: number,
+  count: number,
+  isFeature: boolean = false
+) => {
+  const filter = (node: MarkdownRemark) => {
+    if (!isFeature) {
+      return node.frontmatter!.feature !== true // Could be null
+    }
+    return node.frontmatter!.feature === true
+  }
+
+  return edges
+    .filter(({ node }) => filter(node))
+    .slice(start, count)
+    .map(({ node }) => createSummary(node, isFeature))
+}
+
 const BlogPage = ({ data, pageContext }: Props) => {
   const [t] = useTranslation()
   const [blogs, setBlogs] = useState<SummaryProps[]>([])
 
-  console.log(data)
+  const edges = data.allMarkdownRemark!.edges
 
-  const filtered = data.allMarkdownRemark!.edges.filter(
-    (edge) =>
-      edge!.node!.frontmatter!.featuredImage != undefined ||
-      edge!.node!.frontmatter!.featuredImage != null
-  )
+  const summaries = getBlogs(edges, 0, edges.length, true)
+  const latests = getBlogs(edges, 0, 4, false)
 
-  const summaries = data
-    .allMarkdownRemark!.edges.filter(
-      ({ node }) => node.frontmatter!.feature === true
-    )
-    .map(({ node }) => {
-      const { title, date, description } = node.frontmatter!
-      return {
-        title: title as string,
-        date: date as string,
-        description: description as string,
-        slug: node!.fields!.slug as string,
-        img: node!.frontmatter!.featuredImage.childrenImageSharp[0].fluid,
-      }
-    })
-
-  const latests = filtered
-    .filter(({ node }) => node.frontmatter!.feature !== true)
-    .slice(0, 4)
-    .map(({ node }) => {
-      const { title, date } = node.frontmatter!
-      return {
-        title: title as string,
-        date: date as string,
-        slug: node!.fields!.slug as string,
-        img: node!.frontmatter!.featuredImage.childrenImageSharp[0].fluid,
-      }
-    })
-
-  const maxCount = filtered
+  const maxCount = edges
     .filter(({ node }) => node.frontmatter!.feature !== true)
     .slice(4).length
 
-  const onSeeMore = () => {
+  const setBlogList = () => {
     const count = maxCount - blogs.length > 8 ? blogs.length + 8 : maxCount
-    console.log(count)
-    setBlogs(
-      filtered
-        .filter(({ node }) => node.frontmatter!.feature !== true)
-        .slice(4, 4 + count)
-        .map(({ node }) => {
-          const { title, date } = node.frontmatter!
-          return {
-            title: title as string,
-            date: date as string,
-            slug: node!.fields!.slug as string,
-            img: node!.frontmatter!.featuredImage.childrenImageSharp[0].fluid,
-          }
-        })
-    )
-    console.log('Hello', blogs)
+    setBlogs(getBlogs(edges, 4, 4 + count, false))
   }
 
   useEffect(() => {
-    const count = maxCount - blogs.length > 8 ? blogs.length + 8 : maxCount
-
-    console.log(filtered)
-    setBlogs(
-      filtered
-        .filter(({ node }) => node.frontmatter!.feature !== true)
-        .slice(4, 4 + count)
-        .map(({ node }) => {
-          const { title, date } = node.frontmatter!
-          return {
-            title: title as string,
-            date: date as string,
-            slug: node!.fields!.slug as string,
-            img: node!.frontmatter!.featuredImage.childrenImageSharp[0].fluid,
-          }
-        })
-    )
+    setBlogList()
   }, [])
 
   return (
@@ -172,7 +153,7 @@ const BlogPage = ({ data, pageContext }: Props) => {
         />
         {maxCount > blogs.length && (
           <div
-            onClick={onSeeMore}
+            onClick={setBlogList}
             className="bg-blue-600 text-center text-gray-100 md:w-6/12 xs:w-36 py-2 cursor-pointer"
           >
             <div>{t('see-more')}</div>
