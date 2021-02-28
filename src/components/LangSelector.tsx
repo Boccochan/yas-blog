3ad62da0c2baf2d42ae6ebe794e806e478789a9c
Yasuhiro <boccochan@gmail.com>
2021-02-28 22:32:44 +0900

    Disable when the language is seledted

--- src/components/LangSelector.tsx
+++ src/components/LangSelector.tsx
@@ -1,18 +1,47 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Link as GatsbyLink } from 'gatsby'
 import { usePageContext } from '@/i18n/PageContext'
 import { GrLanguage } from 'react-icons/gr'
 import { useTranslation } from 'react-i18next'
 
+const langs = {
+  en: {
+    selected: 'English',
+    ja: 'text-black',
+    en: 'text-gray-500 cursor-default',
+  },
+  ja: {
+    selected: '日本語',
+    ja: 'text-gray-500 cursor-default',
+    en: 'text-black',
+  },
+}
+
 const LangSelector = () => {
   const [isOpen, setOpen] = useState(false)
   const { originalPath } = usePageContext()
   const { i18n } = useTranslation()
-  const [lang, setLang] = useState('English')
+  const [lang, setLang] = useState(langs.en)
+  const menuRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    setLang(i18n.language === 'ja' ? '日本語' : 'English')
-  }, [i18n.language])
+    setLang(i18n.language === 'ja' ? langs.ja : langs.en)
+
+    function handleClickOutside(event) {
+      if (
+        menuRef !== null &&
+        menuRef.current &&
+        !menuRef.current.contains(event.target)
+      ) {
+        setOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [i18n.language, menuRef])
 
   return (
     <div className="relative inline-block text-left h-full">
@@ -21,10 +50,13 @@ const LangSelector = () => {
         className="flex item-center h-full cursor-pointer"
       >
         <GrLanguage />
-        <p className="hidden md:block ml-2 text-xs">{lang}</p>
+        <p className="hidden md:block ml-2 text-xs">{lang.selected}</p>
       </div>
       {isOpen && (
-        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50">
+        <div
+          ref={menuRef}
+          className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50"
+        >
           <div
             className="py-1"
             role="menu"
@@ -34,7 +66,7 @@ const LangSelector = () => {
             <GatsbyLink
               aria-label={`Change language to ja`}
               to={`/ja${originalPath}`}
-              className="w-full block ml-2"
+              className={`w-full block ml-2 ${lang.ja}`}
             >
               日本語
             </GatsbyLink>
@@ -42,7 +74,7 @@ const LangSelector = () => {
             <GatsbyLink
               aria-label={`Change language to en`}
               to={`/en${originalPath}`}
-              className="w-full block  ml-2"
+              className={`w-full block ml-2 ${lang.en}`}
             >
               English
             </GatsbyLink>
