6f99048bff551fbca89097aa34a82d5f30ca1fea
Yasuhiro Ito <boccochan@gmail.com>
2021-03-21 00:02:00 +0900

    Add prev and next

--- src/template/createPosts.tsx
+++ src/template/createPosts.tsx
@@ -1,11 +1,4 @@
 import path from 'path'
-import { graphql, PageRendererProps, useStaticQuery } from 'gatsby'
-import {
-  Query,
-  SitePageContext,
-  MarkdownRemark,
-  MarkdownRemarkEdge,
-} from '@/types/graphql-types'
 
 interface Post {
   node: {
@@ -41,61 +34,14 @@ type GatsbyCreatePages = (fns: {
   boundActionCreators: BoundActionCreators
 }) => void
 
-export const createPages: GatsbyCreatePages = async ({
-  graphql,
-  boundActionCreators,
-}) => {
-  const { createPage } = boundActionCreators
-
-  const allMarkdown = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { fields: [frontmatter___date], order: DESC }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-              date
-              description
-              lang
-              feature
-              featuredImage {
-                childrenImageSharp {
-                  fluid(maxWidth: 800, maxHeight: 500) {
-                    aspectRatio
-                    base64
-                    sizes
-                    src
-                    srcSet
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  if (allMarkdown.errors) {
-    throw allMarkdown.errors
-  }
-
-  // Create blog posts pages.
-  const posts = allMarkdown.data.allMarkdownRemark.edges
-
+const createPosts = (
+  posts: Post[],
+  lang: string,
+  createPage: (page: PageInput) => void
+) => {
   posts.forEach((post: Post, index: number) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node
     const next = index === 0 ? null : posts[index - 1].node
-    const lang =
-      post.node.frontmatter.lang === undefined
-        ? 'ja'
-        : post.node.frontmatter.lang
 
     createPage({
       path: `/${lang}/blogs${post.node.fields.slug}`,
@@ -109,6 +55,71 @@ export const createPages: GatsbyCreatePages = async ({
       },
     })
   })
+}
+
+const query = `
+{
+  allMarkdownRemark(
+    sort: { fields: [frontmatter___date], order: DESC }
+    limit: 1000
+  ) {
+    edges {
+      node {
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          date
+          description
+          lang
+          feature
+          featuredImage {
+            childrenImageSharp {
+              fluid(maxWidth: 800, maxHeight: 500) {
+                aspectRatio
+                base64
+                sizes
+                src
+                srcSet
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+export const createPages: GatsbyCreatePages = async ({
+  graphql,
+  boundActionCreators,
+}) => {
+  const { createPage } = boundActionCreators
+
+  const allMarkdown = await graphql(query)
+
+  if (allMarkdown.errors) {
+    throw allMarkdown.errors
+  }
+
+  // Create blog posts pages.
+  const posts = allMarkdown.data.allMarkdownRemark.edges
+
+  const en = posts.filter(
+    (post) =>
+      post.node.frontmatter.lang != null && post.node.frontmatter.lang === 'en'
+  )
+
+  createPosts(en, 'en', createPage)
+
+  const ja = posts.filter(
+    (post) =>
+      post.node.frontmatter.lang != null && post.node.frontmatter.lang === 'ja'
+  )
+
+  createPosts(ja, 'ja', createPage)
 
   return null
 }
