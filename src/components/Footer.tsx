fddf15ad8ae64344fbde6790a065915523e7a0d2
Yasuhiro Ito <boccochan@gmail.com>
2021-03-20 17:09:59 +0900

    Refactor footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer id="footer" className="flex-none bg-gray-300 sticky top-0 mt-10">
+    <p className="p-2 text-center text-xs">
+      Copyright © 2021 Yasuhiro Ito. All Rights Reserved.
+    </p>
+  </footer>
+)
+
+export default Footer
